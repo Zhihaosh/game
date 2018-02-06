93f96315a76cee8d7670a1d72b6c14087f4c0417
单志昊 <danzhihao@danzhihaodeMacBook-Pro.local>
2018-02-05 16:24:38 -0800

    2

---
+++ NewProject/Assets/game/camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+	public float speed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveCamera ();
+	}
+	void MoveCamera()
+	{
+		var oldY = this.gameObject.transform.localPosition;
+		oldY.y = oldY.y - speed * Time.deltaTime;
+		this.gameObject.transform.localPosition = oldY;
+	}
+}
