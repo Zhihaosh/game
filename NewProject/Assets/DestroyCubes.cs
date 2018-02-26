711214f1a2f9b9ccefe398518726edbc99c3d270
单志昊 <danzhihao@danzhihaodeMacBook-Pro.local>
2018-02-26 15:34:52 -0800

    break

---
+++ NewProject/Assets/DestroyCubes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCubes : MonoBehaviour
+{
+	void OnCollisionEnter2D (Collision2D col)
+	{
+
+		Destroy(this.gameObject);
+
+	}
+}
